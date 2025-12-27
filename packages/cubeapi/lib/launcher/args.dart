5385d122f9e363945715ffa5c3292ddcf9d4822d
Eric Apostal <ehapostal04@gmail.com>
2025-12-27 14:24:00 -0500

    maybe parse details

--- packages/cubeapi/lib/launcher/args.dart
+++ packages/cubeapi/lib/launcher/args.dart
@@ -1,1 +1,47 @@
 // just an idea for structure, could make sense for logic
+import 'package:cubeapi/cubeapi.dart';
+
+String parseVersionDetails(
+  VersionDetails rawDetails, {
+  required String authPlayerName,
+  required String versionName,
+  required String gameDirectory,
+  required String assetsRoot,
+  required String assetsIndexName,
+  required String authUuid,
+  required String authAccessToken,
+  required String clientId,
+  required String authXuid,
+  required String versionType,
+  required int resolutionWidth,
+  required int resolutionHeight,
+  required String nativesDirectory,
+  required String classPath,
+  required String logConfigPath,
+}) {
+  final stringData = rawDetails.toJson();
+  final parsedData = stringData
+      .replaceAll(r"${auth_player_name}", authPlayerName)
+      .replaceAll(r"${version_name}", versionName)
+      .replaceAll(r"${game_directory}", gameDirectory)
+      .replaceAll(r"${assets_root}", assetsRoot)
+      .replaceAll(r"${assets_index_name}", assetsIndexName)
+      .replaceAll(r"${auth_uuid}", authUuid)
+      .replaceAll(r"${auth_access_token}", authAccessToken)
+      .replaceAll(r"${clientid}", clientId)
+      .replaceAll(r"${auth_xuid}", authXuid)
+      .replaceAll(r"${version_type}", versionType)
+      .replaceAll(r"${resolution_width}", resolutionWidth.toString())
+      .replaceAll(r"${resolution_height}", resolutionHeight.toString())
+      .replaceAll(r"${quickPlayPath}", "")
+      .replaceAll(r"${quickPlaySingleplayer}", "")
+      .replaceAll(r"${quickPlayMultiplayer}", "")
+      .replaceAll(r"${quickPlayRealms}", "")
+      .replaceAll(r"${natives_directory}", nativesDirectory)
+      .replaceAll(r"${launcher_name}", "cubestrap")
+      .replaceAll(r"${launcher_version}", "0.0.1")
+      .replaceAll(r"${classpath}", classPath)
+      .replaceAll(r"${path}", logConfigPath);
+
+  return parsedData;
+}
